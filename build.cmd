f694e90f636d7b28aaa309a408bc8fdcdd3f787f
filou56 <filouwelle@gmail.com>
2024-03-14 20:29:57 +0100

    Windows build 2

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
-\opt\miniconda3\Scripts\pyinstaller.exe elzwelle_sheet.py
-copy \opt\miniconda3\Library\bin\libcrypto-3-x64.dll dist\elzwelle_sheet\_internal
-copy \opt\miniconda3\Library\bin\libssh2.dll dist\elzwelle_sheet\_internal
+copy ..\elzwelle_modbus_start.py .
+\opt\miniconda3\Scripts\pyinstaller.exe elzwelle_modbus_start.py
+copy \opt\miniconda3\Library\bin\libcrypto-3-x64.dll dist\elzwelle_modbus_start\_internal
+copy \opt\miniconda3\Library\bin\libssh2.dll dist\elzwelle_modbus_start\_internal
